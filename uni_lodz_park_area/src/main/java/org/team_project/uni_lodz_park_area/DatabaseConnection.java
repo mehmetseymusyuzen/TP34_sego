2264e986adb0da4656c264552728b79ce99d0c31
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-11-21 15:51:19 +0100

    pom.xml has been changed, we deleted mysql-j-connector dependency and related class

--- uni_lodz_park_area/src/main/java/org/team_project/uni_lodz_park_area/DatabaseConnection.java
+++
@@ -1,5 +0,0 @@
-package org.team_project.uni_lodz_park_area;
-
-public class DatabaseConnection {
-
-}
--- uni_lodz_park_area/src/main/java/org/team_project/uni_lodz_park_area/UniLodzParkAreaApplication.java
+++ uni_lodz_park_area/src/main/java/org/team_project/uni_lodz_park_area/UniLodzParkAreaApplication.java
@@ -3,39 +3,11 @@ package org.team_project.uni_lodz_park_area;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 @SpringBootApplication
 public class UniLodzParkAreaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniLodzParkAreaApplication.class, args);
-        String url = "jdbc:mysql://localhost:3306/park";
-        String username = "root";
-        String password = "root1234";
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("MySQL connection successful!");
-
-            String sql = "SELECT * FROM park_area";
-            try (PreparedStatement statement = connection.prepareStatement(sql);
-                 ResultSet resultSet = statement.executeQuery()) {
-
-                while (resultSet.next()) {
-                    String park_area_msg = resultSet.getString("park_area_msg");
-                    System.out.println(park_area_msg);
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection failed!");
-            e.printStackTrace();
-        }
     }
 
 }
