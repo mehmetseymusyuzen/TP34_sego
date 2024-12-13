c0a729e8ee011b979fd3fe800cc9e74fea19593f
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-12-13 22:43:13 +0100

    Create project common model and entity

---
+++ uni_lodz_park_area/src/main/java/org/team_project/uni_lodz_park_area/common/model/BaseDomainModel.java
@@ -0,0 +1,26 @@
+package org.team_project.uni_lodz_park_area.common.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+/**
+ * Base Abstract Class of Domain Model Objects.
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class BaseDomainModel {
+    protected String createdUser;
+    protected LocalDateTime createdAt;
+    protected String updatedUser;
+    protected LocalDateTime updatedAt;
+}
