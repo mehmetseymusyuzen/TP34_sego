44583883a35823bf1169f08f96c95c1ae08ed3b3
Dc.L <luodongchen@gmail.com>
2024-12-14 15:39:03 +0100

    priceList upload

---
+++ uni_lodz_park_area/src/main/java/org/team_project/uni_lodz_park_area/model/dto/request/priceList/PriceListCreateRequest.java
@@ -0,0 +1,26 @@
+package org.team_project.uni_lodz_park_area.model.dto.request.priceList;
+
+import org.team_project.uni_lodz_park_area.model.dto.request.priceList.PriceListCreateRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * A request class named {@link PriceListCreateRequest} representing the creation of a price list.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceListCreateRequest {
+    @NotBlank
+    private String name;
+
+    @Valid
+    private List<PriceCreateRequest> prices;
+}
