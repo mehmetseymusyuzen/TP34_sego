067bcc473ae8c4a25bd0148a746342d3b40a58a0
mustafacapras <mustafaacapras@gmail.com>
2024-12-15 16:33:15 +0100

    logging implementation done and Builder design pattern infrastructure was created for the test infrastructure.

--- uni_lodz_park_area/src/test/java/org/team_project/uni_lodz_park_area/builder/ParkingAreaUpdateRequestBuilder.java
+++ uni_lodz_park_area/src/test/java/org/team_project/uni_lodz_park_area/builder/ParkingAreaUpdateRequestBuilder.java
@@ -1,2 +1,26 @@
-package org.team_project.uni_lodz_park_area.builder;public class ParkingAreaUpdateRequestBuilder {
+package com.project.parkinglot.builder;
+
+import com.github.javafaker.Faker;
+import com.project.parkinglot.model.dto.request.parking_area.ParkingAreaUpdateRequest;
+
+public class ParkingAreaUpdateRequestBuilder extends BaseBuilder<ParkingAreaUpdateRequest>{
+
+    public ParkingAreaUpdateRequestBuilder() {super(ParkingAreaUpdateRequest.class);}
+
+    public ParkingAreaUpdateRequestBuilder withValidFields(){
+        final Faker faker = new Faker();
+        return this
+                .withCapacity(faker.number().numberBetween(1,23));
+    }
+
+    public ParkingAreaUpdateRequestBuilder withCapacity(
+            final Integer capacity
+    ){
+        data.setCapacity(capacity);
+        return this;
+    }
+
+    @Override
+    public ParkingAreaUpdateRequest build(){return super.build();}
+
 }
